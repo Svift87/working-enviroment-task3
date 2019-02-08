1e1e5744eccc8743290ba9d5de24f8c61d06c1ec
Sergey <kuryakov-s@mail.ru>
2019-02-08 21:02:17 +0700

    fix #2

---
+++ dist/app.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/* eslint-disable no-unused-vars */
+var characters = [{
+  name: 'мечник',
+  health: 10
+}, {
+  name: 'маг',
+  health: 100
+}, {
+  name: 'маг',
+  health: 0
+}, {
+  name: 'лучник',
+  health: 0
+}];
+var alive = characters.filter(function (item) {
+  return item.health > 0;
+});
